3e55294c673e9e67b2bdfbc38cf48516457b681d
ahmadshakir21 <ahmad.software.developer@gmail.com>
2023-12-16 20:31:15 +0300

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyApplication(),
+    );
+  }
+}
+
+class MyApplication extends StatefulWidget {
+  const MyApplication({super.key});
+
+  @override
+  State<MyApplication> createState() => _MyApplicationState();
+}
+
+class _MyApplicationState extends State<MyApplication> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
